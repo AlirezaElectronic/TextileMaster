f74ae2ad9b46d972c1ab90f7b8334498dbd1e177
Alireza <alireza.sarrami.electronic@gmail.com>
2025-09-30 12:11:13 +0330

    DMA2D worked

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "imag.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -70,6 +70,8 @@ FMC_SDRAM_CommandTypeDef command; //Control command
 
 /* Private variables ---------------------------------------------------------*/
 
+DMA2D_HandleTypeDef hdma2d;
+
 LTDC_HandleTypeDef hltdc;
 
 SDRAM_HandleTypeDef hsdram1;
@@ -84,9 +86,13 @@ void PeriphCommonClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_FMC_Init(void);
 static void MX_LTDC_Init(void);
+static void MX_DMA2D_Init(void);
 /* USER CODE BEGIN PFP */
 void MPU_Config(void);
 void FillFramebuffer(uint32_t framebuffer_addr, uint16_t color, uint16_t width, uint16_t height);
+
+
+uint16_t *fb_ptr = (uint16_t *)FRAMEBUFFER_ADDR;
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -141,6 +147,10 @@ void SDRAM_Initialization_Sequence(SDRAM_HandleTypeDef *hsdram, FMC_SDRAM_Comman
 
 }
 
+
+
+
+uint16_t  *DataTOLCD = (uint16_t*)FRAMEBUFFER_ADDR ;
 /* USER CODE END 0 */
 
 /**
@@ -185,6 +195,7 @@ int main(void)
   MX_GPIO_Init();
   MX_FMC_Init();
   MX_LTDC_Init();
+  MX_DMA2D_Init();
   /* USER CODE BEGIN 2 */
   HAL_GPIO_WritePin(LCD_BL_GPIO_Port, LCD_BL_Pin, 1);
 
@@ -233,25 +244,55 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+//
+//		FillFramebuffer(FRAMEBUFFER_ADDR, COLOR_RED, LCD_WIDTH, LCD_HEIGHT);
+//		HAL_Delay(500); // Wait 2 seconds
+//
+//		// Fill with green
+//		FillFramebuffer(FRAMEBUFFER_ADDR, COLOR_GREEN, LCD_WIDTH, LCD_HEIGHT);
+//		HAL_Delay(500);
+//
+//		// Fill with blue
+//		FillFramebuffer(FRAMEBUFFER_ADDR, COLOR_BLUE, LCD_WIDTH, LCD_HEIGHT);
+//		HAL_Delay(500);
+//
+//		// Fill with white
+//		FillFramebuffer(FRAMEBUFFER_ADDR, COLOR_WHITE, LCD_WIDTH, LCD_HEIGHT);
+//		HAL_Delay(500);
+//
+//		// Fill with black
+//		FillFramebuffer(FRAMEBUFFER_ADDR, COLOR_BLACK, LCD_WIDTH, LCD_HEIGHT);
+//		HAL_Delay(500);
+//
+//	    for(int i =0 ;i  < sizeof(gImage_TestImage) / 2  ;i++){
+//	    	fb_ptr[i] = gImage_TestImage[i] | gImage_TestImage[i+1];
+//	    }
 
-		FillFramebuffer(FRAMEBUFFER_ADDR, COLOR_RED, LCD_WIDTH, LCD_HEIGHT);
-		HAL_Delay(500); // Wait 2 seconds
+//     for(int i =0  ; i< sizeof(IMG_20250926_153935 ) ;i++){
+//    	 DataTOLCD[i] = IMG_20250926_153935[i];
+//    	 i++;
+//     }
+//	  HAL_DMA2D_Start(&hdma2d, (uint32_t)IMG_20250926_153935, FRAMEBUFFER_ADDR, 480, 272);
+//	  HAL_DMA2D_PollForTransfer(&hdma2d, 100);
+//
+//	  HAL_Delay(500);
+//
+//	  HAL_DMA2D_Start(&hdma2d, (uint32_t)AliImage1, FRAMEBUFFER_ADDR, 480, 272);
+//	  HAL_DMA2D_PollForTransfer(&hdma2d, 100);
+//	  HAL_Delay(500);
 
-		// Fill with green
-		FillFramebuffer(FRAMEBUFFER_ADDR, COLOR_GREEN, LCD_WIDTH, LCD_HEIGHT);
-		HAL_Delay(500);
+	  HAL_DMA2D_Start(&hdma2d, (uint32_t)AlireImage2, FRAMEBUFFER_ADDR, 480, 272);
+	  HAL_DMA2D_PollForTransfer(&hdma2d, 100);
 
-		// Fill with blue
-		FillFramebuffer(FRAMEBUFFER_ADDR, COLOR_BLUE, LCD_WIDTH, LCD_HEIGHT);
-		HAL_Delay(500);
-
-		// Fill with white
-		FillFramebuffer(FRAMEBUFFER_ADDR, COLOR_WHITE, LCD_WIDTH, LCD_HEIGHT);
-		HAL_Delay(500);
-
-		// Fill with black
-		FillFramebuffer(FRAMEBUFFER_ADDR, COLOR_BLACK, LCD_WIDTH, LCD_HEIGHT);
-		HAL_Delay(500);
+	  HAL_Delay(500);
+//     HAL_Delay(500) ;
+//
+//
+//     for(int i =0  ; i< 272 * 480 ;i++){
+//    	 DataTOLCD[i] = COLOR_GREEN;
+//     }
+//
+//     HAL_Delay(500) ;
 
 	  GeneralCounter ++ ;
     /* USER CODE END WHILE */
@@ -337,6 +378,46 @@ void PeriphCommonClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief DMA2D Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_DMA2D_Init(void)
+{
+
+  /* USER CODE BEGIN DMA2D_Init 0 */
+
+  /* USER CODE END DMA2D_Init 0 */
+
+  /* USER CODE BEGIN DMA2D_Init 1 */
+
+  /* USER CODE END DMA2D_Init 1 */
+  hdma2d.Instance = DMA2D;
+  hdma2d.Init.Mode = DMA2D_M2M;
+  hdma2d.Init.ColorMode = DMA2D_OUTPUT_RGB565;
+  hdma2d.Init.OutputOffset = 0;
+  hdma2d.LayerCfg[1].InputOffset = 0;
+  hdma2d.LayerCfg[1].InputColorMode = DMA2D_INPUT_RGB565;
+  hdma2d.LayerCfg[1].AlphaMode = DMA2D_REPLACE_ALPHA;
+  hdma2d.LayerCfg[1].InputAlpha = 255;
+  hdma2d.LayerCfg[1].AlphaInverted = DMA2D_REGULAR_ALPHA;
+  hdma2d.LayerCfg[1].RedBlueSwap = DMA2D_RB_REGULAR;
+  hdma2d.LayerCfg[1].ChromaSubSampling = DMA2D_NO_CSS;
+  if (HAL_DMA2D_Init(&hdma2d) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_DMA2D_ConfigLayer(&hdma2d, 1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN DMA2D_Init 2 */
+
+  /* USER CODE END DMA2D_Init 2 */
+
 }
 
 /**
@@ -537,7 +618,7 @@ void MPU_Config(void)
 
 void FillFramebuffer(uint32_t framebuffer_addr, uint16_t color, uint16_t width, uint16_t height) {
     uint32_t num_pixels = width * height;
-    uint16_t *fb_ptr = (uint16_t *)framebuffer_addr;
+
 
     for (uint32_t i = 0; i < num_pixels; i++) {
         fb_ptr[i] = color;
